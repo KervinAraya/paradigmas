c423160e8ca6bfa220310be2ca2bb43eaf64a7a5
KervinAraya <arayaromerokervin@gmail.com>
2017-08-02 16:34:33 -0600

    Berny

--- business/agregarPersonaAction.php
+++ business/agregarPersonaAction.php
@@ -1,3 +1,7 @@
 <?php
+    
+    include_once '../dominio/Persona.php';
+    $personaId = $_POST['personaId'];
+    $persona = new Persona($personaId, $personaNombre, $personaApellido1, $personaApellido2, $personaTelefono, $personaDireccion, $personaCorreo)
 
 ?>
