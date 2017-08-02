8e0ed19838fbcb0dc1679c4c4286c5ba638e6cb5
NanaOva <nathy.ovares@gmail.com>
2017-08-02 16:19:26 -0600

    cambios en persona

--- dominio/Persona.php
+++ dominio/Persona.php
@@ -3,8 +3,28 @@
 class Persona {
    
     private $personaId;
+    private $personaNombre;
+    private $personaApellido1;
+    private $personaApellido2;
+    private $personaTelefono;
+    private $personaDireccion;
+    private $personaCorreo;
     
     
-}
+    public function __construct($personaId,$personaNombre,$personaApellido1,$personaApellido2,$personaTelefono,
+            $personaDireccion,$personaCorreo){
+        $this->personaId;
+        $this->personaNombre = $personaNombre;
+        $this->personaApellido1 = $personaApellido1;
+        $this->personaApellido2 = $personaApellido2;
+        $this->personaTelefono = $personaTelefono;
+        $this->personaDireccion = $personaDireccion;
+        $this->personaCorreo = $personaCorreo;
+ }
+ }
+            
+    
+    
+
 
 ?>
