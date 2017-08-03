27da6bd5afd687e914b9e6634bc8e9b56253e49a
prueba <prueba@berny>
2017-08-02 18:13:50 -0600

    validaciones de parte de Businnes

--- business/agregarPersonaAction.php
+++ business/agregarPersonaAction.php
@@ -1,7 +1,23 @@
 <?php
     
     include_once '../dominio/Persona.php';
+    
     $personaId = $_POST['personaId'];
-    $persona = new Persona($personaId, $personaNombre, $personaApellido1, $personaApellido2, $personaTelefono, $personaDireccion, $personaCorreo)
-
+    $personaNombre = $_POST['personaNombre'];
+    $personaApellido1 = $_POST['personaApellido1'];
+    $personaApellido2 = $_POST['personaApellido2'];
+    $personaTelefono = $_POST['personaTelefono'];
+    $personaDirecccion = $_POST['personaDireccion'];
+    $personaCorreo = $_POST['personaCorreo'];
+    
+    //Validacciones para ver si creo el objeto persona o no..(si no es vacio el espacio)
+    
+    if(isset($personaId) && isset($personaNombre) && isset($personaApellido1) && isset($personaApellido2onaId) && isset($personaTelefono) && isset($personaDirecccion) && isset($personaCorreo)){
+       $persona = new Persona($personaId, $personaNombre, $personaApellido1, $personaApellido2, $personaTelefono, $personaDireccion, $personaCorreo); 
+    }else{
+        header("Location:../view/agregarPersonaView.php");
+    }
+    
+    
+            
 ?>
